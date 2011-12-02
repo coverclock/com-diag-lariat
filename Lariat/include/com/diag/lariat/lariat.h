433911d20ba435c2ab13dcf46a55a8193a963052
jsloan <jsloan@493acde8-ba85-0410-ba73-c1b23885ab7e>
2011-12-02 19:51:40 +0000

    Implemented doxygen doc generation.
    git-svn-id: svn://graphite/lariat/trunk@17 493acde8-ba85-0410-ba73-c1b23885ab7e

--- Lariat/include/com/diag/lariat/lariat.h
+++ Lariat/include/com/diag/lariat/lariat.h
@@ -16,6 +16,7 @@ namespace com { namespace diag { namespace lariat {
 /**
  * Convert the string into a binary number suitable for a resource limit value.
  * @param string points to the string.
+ *
  * @param valuep if non-null points to where the value is returned.
  * @return a pointer past the last numeric character of the string.
  */
@@ -24,6 +25,7 @@ extern const char * number(const char * string, unsigned long * valuep = 0);
 /**
  * Print a stack trace to the specified file descriptor using a buffer of
  * the specified size.
+ *
  * @param buffer points to a buffer into which the stack trace data is stored.
  * @param size is the cardinality of the buffer.
  * @param fd is the file descriptor.
@@ -33,6 +35,7 @@ extern int stacktrace(void ** buffer, unsigned int size, int fd = 2);
 
 /**
  * Print a stack trace to standard error using a default buffer.
+ *
  * @return the number of positions used in the default buffer.
  */
 extern int stacktrace();
@@ -49,6 +52,7 @@ static const unsigned long UNLIMITED = ~(unsigned long)0;
  * limit is set to the value. If UNLIMITED is specified, the resource limit is
  * set to the pre-defined hard maximum, or to INFINITY is force is applied.
  * Only privileged processes may apply force.
+ *
  * @param resource identifies the resource.
  * @param value is the new limit value or UNLIMITED.
  * @param force if true overrides the pre-defined maximum hard limit value.
@@ -58,6 +62,7 @@ extern int limit(int resource, unsigned long value = UNLIMITED, bool force = fal
 
 /**
  * Install the signal handler for the specified signal.
+ *
  * @param signum is the signal for which the handler is installed.
  * @param handler points to the signal handler function or NULL to remove.
  * @param restart if true causes interrupted system calls to be restarted.
@@ -69,6 +74,7 @@ extern int install(int signum, void (* handler)(int) = 0, bool restart = false, 
 /**
  * Set a non-periodic interval timer for real clock time in seconds or clear an
  * existing timer.
+ *
  * @param seconds is the timer interval in seconds or zero to clear.
  * @return 0 for success, <0 otherwise.
  */
@@ -80,6 +86,7 @@ extern int timer(unsigned long seconds = 0);
  * a bunch of command line options to place or relax limits on the unit test
  * process. This is useful when running the process as part of a larger, and
  * perhaps automated, unit test suite.
+ *
  * @param argc is the number of command line arguments including the program name.
  * @param argv is a vector of the command line argument strings.
  * @param envp is a vector of the environmental variables.
